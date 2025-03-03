edf71802ee4d27e4e19d35a0cce059a507b33478
Michael Z Daryabeygi <michael@caracolazul.dev>
2025-03-02 19:25:34 -0500

    add all available ansible commands to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,31 +2,67 @@
 
 set -euo pipefail
 
+# /usr/bin/ansible-community
+# /usr/bin/ansible-config
+# /usr/bin/ansible-console
+# /usr/bin/ansible-doc
+# /usr/bin/ansible-galaxy
+# /usr/bin/ansible-inventory
+# /usr/bin/ansible-lint
+# /usr/bin/ansible-playbook
+# /usr/bin/ansible-pull
+# /usr/bin/ansible-test
+# /usr/bin/ansible-vault
+    
 # Check if any arguments are passed
 if [ "$#" -eq 0 ]; then
     exec /bin/bash
 else
     # Determine which Ansible command to execute
     case "$1" in
-        playbook)
+        community)
             shift
-            exec ansible-playbook "$@"
+            exec ansible-community "$@"
             ;;
         config)
             shift
             exec ansible-config "$@"
             ;;
+        console)
+            shift
+            exec ansible-console "$@"
+            ;;
+        doc)
+            shift
+            exec ansible-doc "$@"
+            ;;
         galaxy)
             shift
             exec ansible-galaxy "$@"
             ;;
+        inventory)
+            shift
+            exec ansible-inventory "$@"
+            ;;
+        lint)
+            shift
+            exec ansible-lint "$@"
+            ;;
+        playbook)
+            shift
+            exec ansible-playbook "$@"
+            ;;
+        pull)
+            shift
+            exec ansible-pull "$@"
+            ;;
+        test)
+            shift
+            exec ansible-test "$@"
+            ;;
         vault)
             shift
             exec ansible-vault "$@"
-            ;;
-        console)
-            shift
-            exec ansible-console "$@"
             ;;
         *)
 			shift
